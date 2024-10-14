8b4e5ffa7f3994cfee99a38120f5fa605eb52057
Noskine <noskine@outlook.com>
2024-10-14 14:49:12 -0300

    init api

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	_ "github.com/Noskine/StockSync/internal/config"
+	"github.com/Noskine/StockSync/internal/repository"
 )
 
 func main() {
+	rep := new(repository.UserRepository)
 
+	result, err := rep.FindAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
 }
--- internal/repository/user-repository.go
+++ internal/repository/user-repository.go
@@ -27,3 +27,33 @@ func (ur *UserRepository) Create(e entities.User) (string, error) {
 
 	return id, nil
 }
+
+func (ur *UserRepository) FindAll() ([]entities.User, error) {
+	conn := database.OpenConn()
+	defer conn.Close()
+
+	strQuery := "SELECT * FROM public.users"
+
+	rows, err := conn.Query(strQuery)
+	if err != nil {
+		return nil, errors.New("error fetching data from the database")
+	}
+
+	var users []entities.User
+
+	for rows.Next() {
+		var user entities.User
+
+		err := rows.Scan(&user.Id, &user.Name)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
